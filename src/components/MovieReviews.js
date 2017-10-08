63dc005cc707d9a87c1712b5e8e9d658466aa60e
jprosario <joshuaowens011@gmail.com>
2017-10-07 21:03:39 -0400

    wip

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react'
+
+// class MovieReviews extends React.Component {
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className='review-list'>
+      <ul>
+      reviews.map(review => <li>review</li>)
+      </ul>
+    </div>
+  )
+};
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,20 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react'
+import MovieReviews from './MovieReviews.js'
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor(){
+    super()
+
+    this.state ={
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  render(){
+    return <div className='searchable-movie-reviews'><MovieReviews /></div>
+  }
+}
+
+export default SearchableMovieReviewsContainer;

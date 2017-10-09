53a621649fb0527eb2a6ad33514a4b4d38486b6e
jprosario <joshuaowens011@gmail.com>
2017-10-08 23:16:21 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,12 +2,20 @@
 import React from 'react'
 
 // class MovieReviews extends React.Component {
+const displayReview = ({display_title, summary_short}) => {
+  return (
+    <div key={display_title}>
+      <h3> {display_title} </h3>
+      <h5> {summary_short} </h5>
+    </div>
+  )
+}
 const MovieReviews = ({reviews}) => {
+  console.log(reviews);
+
   return (
     <div className='review-list'>
-      <ul>
-      reviews.map(review => <li>review</li>)
-      </ul>
+      {reviews.map(displayReview)}
     </div>
   )
 };

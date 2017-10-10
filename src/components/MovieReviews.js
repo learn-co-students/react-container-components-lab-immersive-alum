78231b4b95610e8fce449c2a5665f9549ceec1b7
jprosario <joshuaowens011@gmail.com>
2017-10-10 10:55:45 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -4,14 +4,13 @@ import React from 'react'
 // class MovieReviews extends React.Component {
 const displayReview = ({display_title, summary_short}) => {
   return (
-    <div key={display_title}>
+    <div key={display_title} className='review'>
       <h3> {display_title} </h3>
       <h5> {summary_short} </h5>
     </div>
   )
 }
-const MovieReviews = ({reviews}) => {
-  console.log(reviews);
+const MovieReviews = ({reviews}) => { //I dont understand the parameter syntax with {}
 
   return (
     <div className='review-list'>
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,6 +1,10 @@
 // Code SearchableMovieReviewsContainer Here
 import React from 'react'
 import MovieReviews from './MovieReviews.js'
+import 'isomorphic-fetch';
+
+const KEY = '94faf6eeb9f0410c9d9e616cd4d2fc6e'
+let URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${KEY}?query=`
 
 class SearchableMovieReviewsContainer extends React.Component {
   constructor(){
@@ -8,13 +12,51 @@ class SearchableMovieReviewsContainer extends React.Component {
 
     this.state ={
       reviews: [],
-      searchTerm: ''
+      searchTerm: 'lil uzi vert'
     }
+
+    this.updateSearch = this.updateSearch.bind(this)
+    // this.searchReviews = this.searchReviews.bind(this)
   }
 
+  updateSearch(e){
+      this.setState({searchTerm: e.target.details})
+  }
+
+  componentWillMount(){
+    fetch(URL+this.state.searchTerm)
+      .then(res => res.json())
+      .then(data => this.setState({reviews: data.results}))
+  }
+
+  // Why does this not render when called in the render function?
+  // But line 31 renders
+  // searchReviews(){
+  //   return (
+  //     <div>
+  //     <input type='text' />
+  //     </div>
+  //   )
+  // }
+
+
+
   render(){
-    return <div className='searchable-movie-reviews'><MovieReviews /></div>
+    return (
+      <div className='searchable-movie-reviews'>
+        <input type='text' onChange={this.updateSearch} value={this.state.searchTerm} />
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
   }
 }
 
 export default SearchableMovieReviewsContainer;
+
+// notes on the workflow:
+// renders and input
+// input sets the value equal to state prop and listens for a change
+// when a change occurs, that event is fired and updateSearch updates the state's search term
+// then before the component mounts, we query the api with the new search term
+// the results of that query are stored in the state and
+// when that component is rendered, we pass the state in via a `reviews` prop to MovieReviews
